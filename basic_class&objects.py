1b8c46cb22bd19aed01cc27889d2fe9a67071ce2
nirupamgarai00 <nirupamgarai00@gmail.com>
2025-10-26 22:23:19 +0530

    Add files via upload

---
+++ basic_class&objects.py
@@ -0,0 +1,8 @@
+#class is something you see as a blueprint of objects
+class Student:
+    school_name = "SRNMGP"
+
+#object is instance of a class
+
+s1 = Student()
+print(s1.school_name)
---
+++ constractor and self.py
@@ -0,0 +1,6 @@
+class Student: #class
+    # in classes __init__ funtion is exectued when ever the odject is insiated
+    def __init__(self):# self is something that object it self represent it self
+        print("constroctesr is being executed")
+
+s1 = Student()
